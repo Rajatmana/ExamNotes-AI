908c515fb3f91b63bf50cdea461ace900ecf7344
Rajat Mana <139938628+Rajatmana@users.noreply.github.com>
2026-04-19 12:39:17 +0530

    Update server URL to production endpoint

--- ExamNotesAI/client/src/App.jsx
+++ ExamNotesAI/client/src/App.jsx
@@ -11,7 +11,7 @@ import History from './pages/History'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://examnotes-ai-5816.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
