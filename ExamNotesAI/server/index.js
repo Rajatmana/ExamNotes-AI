4044f328869cba80ad0ce711e1d61448cde74f3f
Rajat Mana <139938628+Rajatmana@users.noreply.github.com>
2026-04-19 12:48:10 +0530

    Update index.js

--- ExamNotesAI/server/index.js
+++ ExamNotesAI/server/index.js
@@ -20,7 +20,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://examnotes-ai-client-fun1.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
