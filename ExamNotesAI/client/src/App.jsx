04b6df89b4c5927b743b00c2d301d6067357161f
Rajat Mana <139938628+Rajatmana@users.noreply.github.com>
2026-04-19 13:07:15 +0530

    Update App.jsx

--- ExamNotesAI/client/src/App.jsx
+++ ExamNotesAI/client/src/App.jsx
@@ -11,7 +11,7 @@ import History from './pages/History'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
 
-export const serverUrl = "https://examnotes-ai-5816.onrender.com"
+export const serverUrl = "https://examnotes-ai-server-mbr4.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
